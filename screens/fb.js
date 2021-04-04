8d9c1898033369850e7e23750c091a86ba07d20b
Krishna0622 <66967827+Krishna0622@users.noreply.github.com>
2021-04-04 17:08:16 -0400

    Add files via upload

---
+++ screens/fb.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class FacebookScreen extends React.Component{
+render(){
+    return(
+        <View>
+            <Text>
+                Facebook Screen
+            </Text>
+        </View>
+    );
+}
+}
